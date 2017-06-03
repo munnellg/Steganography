ae69533c37fa72d6355996dd1b85b16c8ef57be5
Gary Munnelly <munnellg@tcd.ie>
2017-06-03 20:06:09 +0100

    Added ability to handle commandline arguments. Program now ensures image has capacity to hold embedded file. File paths are stripped before embedding

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ OBJS = $(wildcard src/*.cpp)
 
 CC = g++
 
-LINKER_FLAGS = -O2 -L/usr/X11R6/lib -lm -lpthread -lX11
+LINKER_FLAGS = -O2 -L/usr/X11R6/lib -lm -lpthread -lX11 -lboost_system -lboost_filesystem
 
 COMPILER_FLAGS = -Wall -g
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,69 @@
 #include <iostream>
 #include <fstream>
 #include <stdint.h>
-#include <stdio.h>
-#include <limits.h>
+#include <sstream>
+#include <climits>
+#include <boost/filesystem.hpp>
+#include <map>
 
 /* some helpful macros for determining things like the number of pixel channels
  * required to encode a unit of information or masks for clearing/setting bits
  * during encoding/retrieval */
-#define BYTES_TO_BITS(x)        ((x)*CHAR_BIT)
+#define BYTES_TO_BITS(x)        ((x) * CHAR_BIT)
 #define NUM_CHANNEL_BITS        BYTES_TO_BITS(sizeof(CHANNEL))
 #define ENCODE_BITS_PER_CHANNEL 2
 #define CHANNEL_BIT_MASK        ((((uint64_t) 1) << ENCODE_BITS_PER_CHANNEL)-1)
 #define CHANNELS_TO_ENCODE(x)   ( BYTES_TO_BITS((x))/ENCODE_BITS_PER_CHANNEL )
 
-/* pixel unit - a single colour channel */
-typedef uint8_t CHANNEL;
+const char* DEFAULT_OUTPUT = "out.png";
+
+/* operating modes of the program */
+enum Mode { EMBED, DECODE };
+enum ArgKey { IMAGE, EMBED_FILE, OUTPUT_FILE };
+
+typedef uint8_t  BYTE;    /* 8 bit unsigned integer */
+typedef uint64_t LONG;    /* 64 bit unsigned integer */
+typedef char     CHAR;    /* single string character */
+typedef uint8_t  CHANNEL; /* pixel unit - a single colour channel */
+
+typedef std::map <ArgKey,char*> ArgMap;
+
+/* decode an image to a file by default */
+Mode g_mode = DECODE;
+
+void
+usage() {
+    std::cout<< "usage: steg [-e FILE | -o FILE] IMAGE" << std::endl;
+    exit(-1);
+}
+
+void
+warn( std::string message ) {
+    std::cout << "WARNING: " << message << std::endl;
+}
+
+void
+die( std::string message ) {
+    std::cout << "ERROR: " << message << std::endl;
+    exit(-1);
+}
 
 void
 next ( cimg_library::CImg<CHANNEL> *img, int &pix, int &channel ) {
-    channel = ( (channel) + 1) % img->spectrum();
-    if(!(channel)) {
-        (pix)++;
+    /* choose next channel from those available */
+    channel = (channel + 1) % img->spectrum();
+    /* advance to next pixel if required */
+    if(!channel) {
+        pix++;
     }
 }
 
 /* embed a unit of information starting at the specified location in the
  * image. This function will update the pix and channel ints to point to the
- * location just after the embedded information after the operation is 
+ * location just after the embedded information once the operation is 
  * complete */
 void
-embed ( cimg_library::CImg<CHANNEL> *img, uint64_t data, size_t bytes,
+embed ( cimg_library::CImg<CHANNEL> *img, LONG data, size_t bytes,
         int &pix, int &channel ) {
 
     /* compute how many channels we'll need to store the data and begin to 
@@ -57,14 +91,14 @@ embed ( cimg_library::CImg<CHANNEL> *img, uint64_t data, size_t bytes,
 
 /* retrieve a unit of information from the specified location in the encoded
  * image. The function will update the values of pix and channel to point to
- * the location just after the end of the retrieved data's location */
-uint64_t
-retrieve ( cimg_library::CImg<uint8_t> *img, size_t bytes, int &pix, 
+ * the location just after the retrieved data's location */
+LONG
+retrieve ( cimg_library::CImg<CHANNEL> *img, size_t bytes, int &pix, 
         int &channel ) {
 
-    uint64_t c = 0;
+    LONG c = 0;
 
-    for( unsigned int i=0; i<CHANNELS_TO_ENCODE(bytes); i++ ) {
+    for( LONG i=0; i<CHANNELS_TO_ENCODE(bytes); i++ ) {
         /* retrieve a channel containing information we need to extract */
         CHANNEL *p = img->data( pix%img->width(), pix/img->width(), 0, 
                 channel );
@@ -95,26 +129,49 @@ embed_file_in_image( std::ifstream &file, std::string filename,
     file.seekg( 0, std::ios::end );
     fsize = file.tellg() - fsize;
     file.seekg(0, std::ios::beg);
-   
+
+    /* strip away any path information from our filename in a platform
+     * undependent way */
+    boost::filesystem::path p(filename);
+    filename = p.filename().string();
+
+    /* before we start writing data to the image, make sure it is large
+     * enough to store the embedded data. An image has a capacity that is 
+     * equal to its area times the number of channels per pixel times the
+     * number of bits we are storing per pixel. This yields a capacity in
+     * bits. We divide by the size of a byte (our smallest unit of storage)
+     * to get the capacity in bytes. This is compared to the size of the file
+     * plus the bits of metadata we include for pulling the data back out.
+     * If requirements exceed available resources then the program fails */
+    LONG embed_size = sizeof(BYTE) + filename.length() + sizeof(LONG) + fsize;
+    LONG image_capacity = 
+        (img->width() * img->height() * img->spectrum() * 
+         ENCODE_BITS_PER_CHANNEL)/(BYTES_TO_BITS(sizeof(BYTE)));
+
+    if( embed_size > image_capacity ) {
+        die("Image not large enough to embed data");
+    }
+
     /* embed the filename and the filename length in the image */
-    embed( img, filename.length(), sizeof(uint8_t), pix, channel );
-    for( int i=0; i<filename.length(); i++ ) {
-        embed( img, filename[i], sizeof(uint8_t), pix, channel );
+    embed( img, filename.length(), sizeof(BYTE), pix, channel );
+    for( LONG i=0; i<filename.length(); i++ ) {
+        embed( img, filename[i], sizeof(CHAR), pix, channel );
     }
 
     /* embed file size in the image */
-    embed( img, fsize, sizeof(uint64_t), pix, channel );
+    embed( img, fsize, sizeof(LONG), pix, channel );
 
     /* embed file data in the image */
-    uint8_t byte;
+    BYTE byte;
     while( !file.eof() ) {
         byte = file.get();
-        embed( img, byte, sizeof(uint8_t), pix, channel );
+        embed( img, byte, sizeof(BYTE), pix, channel );
     } 
 }
 
 void
-retrieve_file_from_image( cimg_library::CImg<CHANNEL> *img ) {
+retrieve_file_from_image( cimg_library::CImg<CHANNEL> *img, 
+        char *output_name = NULL ) {
     int pix=0, channel=0;
     
     std::ofstream out;
@@ -124,15 +181,15 @@ retrieve_file_from_image( cimg_library::CImg<CHANNEL> *img ) {
 
     /* retrieve information about the file we are about to load - file size
      * file name and the length of the file name */
-    fname_len = retrieve( img, sizeof(uint8_t), pix, channel );
+    fname_len = retrieve( img, sizeof(BYTE), pix, channel );
     for( int i=0; i<fname_len; i++ ) {
-        char c = retrieve( img, sizeof(char), pix, channel );
+        char c = retrieve( img, sizeof(CHAR), pix, channel );
         fname += c;
     }
-    fsize = retrieve( img, sizeof(uint64_t), pix, channel );
+    fsize = retrieve( img, sizeof(LONG), pix, channel );
 
     /* open the target output file for writing */
-    out.open(fname.c_str(), std::ios::binary);
+    out.open((output_name)? output_name : fname.c_str(), std::ios::binary);
     if(!out.is_open()) {
         /* handle case where we can't open the file for some reason */
         std::cout << "Unable to open " << fname << " for writing" << std::endl;
@@ -141,8 +198,8 @@ retrieve_file_from_image( cimg_library::CImg<CHANNEL> *img ) {
 
     /* start retrieving file data from the image and writing to the output
      * stream */
-    for( uint64_t i=0; i<fsize; i++ ) {
-        uint8_t byte = retrieve( img, sizeof(uint8_t), pix, channel );
+    for( LONG i=0; i<fsize; i++ ) {
+        BYTE byte = retrieve( img, sizeof(BYTE), pix, channel );
         out << byte;
     }
 
@@ -150,51 +207,174 @@ retrieve_file_from_image( cimg_library::CImg<CHANNEL> *img ) {
     out.close();
 }
 
-int
-main ( int argc, char *argv[] )
-{
-    /*
+/* handles input arguments from the command line. Extracts target file names,
+ * sets up the programs mode of operation and any global configuration options
+ * which the user has deigned to change */
+ArgMap
+handle_args ( int argc, char *argv[] ) {
+    ArgMap args;
+    for( int i=1; i<argc; i++ ) {
+        /* any argument which begins with a dash is presumed to be a flag. We
+         * parse the flag as appropriate */
+        if(argv[i][0] == '-') {
+            switch(argv[i][1]) {
+                case 'e':
+                    /* the e flag sets up the program to run in embed mode. It
+                     * also expects that the user has passed an input file to
+                     * be embedded. If no argument follows the -e flag then
+                     * the user has neglected to specify the file they wish
+                     * to embed and the program's operation terminates */
+                    if(i+1 >= argc) {
+                        std::ostringstream oss;
+                        oss << argv[i] << " expects an argument";
+                        die(oss.str());
+                    }
+                    
+                    i++;
+                    g_mode = EMBED;
+                    args[EMBED_FILE] = argv[i];
+                    break;
+                case 'o':
+                    /* the o flag specifies the name of the output file. If 
+                     * running in decode mode, then this will be the name of 
+                     * the file extracted from the image. If running in embed
+                     * mode then this will be the name of the output image
+                     * produced by the program. If no argument follows the -o
+                     * flag then the user has neglected to specify an output
+                     * file name and a warning will be issued. The program 
+                     * will then proceed to use default output names */
+                    if(i+1 >= argc) {
+                        std::ostringstream oss;
+                        oss << argv[i] << " expects an argument";
+                        warn(oss.str());
+                    } else {
+                        i++;
+                        args[OUTPUT_FILE] = argv[i];
+                    }             
+                    break;
+                default:
+                    /* user tried to use a flag that the program does not
+                     * support. Issue a warning and proceed */
+                    std::ostringstream oss;
+                    oss << "Invalid flag: " << argv[i];
+                    warn(oss.str());
+            }            
+        } else {
+            /* any argument which does not begin with a dash (and is not an
+             * argument to some other existing flag)  must be an input file to 
+             * the program. At present this should really only be the input 
+             * image file, but in future there could be more */
+            ArgMap::iterator it = args.find(IMAGE);
+            if(it != args.end()) {
+                std::ostringstream oss;
+                oss << "Already have input image. Ignoring " << argv[i] << 
+                    " and continuing with " << it->second;
+                warn(oss.str());
+            } else {
+                args[IMAGE] = argv[i];
+            }
+        }
+    }
+    return args;
+}
+
+void
+run_embed_mode( ArgMap args ) {
+    char *image_name;
+    char *filename;
+    char *output_name;
+
+    ArgMap::iterator it = args.find(IMAGE);
+    if(it == args.end()) {
+        usage();
+    } 
+
+    image_name = it->second;
+
+    it = args.find(EMBED_FILE);
+    if(it == args.end()) {
+        usage();
+    } 
+
+    filename = it->second;
+
+    it = args.find(OUTPUT_FILE);
+    if(it == args.end()) {
+        output_name = const_cast<char*> (DEFAULT_OUTPUT);
+    } else {
+        output_name = it->second;
+    }
+
     std::ifstream in;
     cimg_library::CImg<CHANNEL> img;
 
-    if( argc != 3 ) {
-        std::cout << "usage: steg FILE IMG" << std::endl;
-        return EXIT_FAILURE;    
-    }
-
-    in.open(argv[1], std::ios::binary);
+    in.open(filename, std::ios::binary);
     if(!in.is_open()) {
-        std::cout << "unable to open " << argv[1] << std::endl;
-        return EXIT_FAILURE;
+        std::ostringstream oss;
+        oss << "unable to open " << filename;
+        die(oss.str());
     }
 
     try {
-        img.load(argv[2]);
+        img.load(image_name);
     } catch ( cimg_library::CImgIOException e ) {
-        return EXIT_FAILURE;
+        std::ostringstream oss;
+        oss << "unable to open " << image_name;
+        die(oss.str());
     }
 
-    std::cout << "ENCODING... ";
-    std::cout.flush();
-    embed_file_in_image( in, "test.txt", &img);
-    std::cout << "OK" << std::endl;
-    
-    img.save("out.png");
-*/
+    embed_file_in_image( in, filename, &img);
+
+    img.save(output_name);
+
+    in.close();
+}
+
+void
+run_decode_mode( ArgMap args ) {
+    char *image_name;
+    char *output_name;
+
+    ArgMap::iterator it = args.find(IMAGE);
+    if(it == args.end()) {
+        usage();
+    } 
+
+    image_name = it->second;
+
+    it = args.find(OUTPUT_FILE);
+    if(it == args.end()) {
+        output_name = NULL;
+    } else {
+        output_name = it->second;
+    }
+
     cimg_library::CImg<CHANNEL> img;
-
-    if( argc != 2 ) {
-        std::cout << "usage: steg IMG" << std::endl;
-        return EXIT_FAILURE;    
-    }
-
     try {
-        img.load(argv[1]);
+        img.load(image_name);
     } catch ( cimg_library::CImgIOException e ) {
-        return EXIT_FAILURE;
+        std::ostringstream oss;
+        oss << "unable to open " << image_name;
+        die(oss.str());
     }
 
-    retrieve_file_from_image( &img);
+    retrieve_file_from_image( &img, output_name );
+}
+
+
+int
+main ( int argc, char *argv[] )
+{
+    ArgMap args = handle_args( argc, argv );
+
+    switch(g_mode) {
+        case EMBED:
+            run_embed_mode(args);
+            break;
+        case DECODE:
+            run_decode_mode(args); 
+            break;
+    }
 
     return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
